5542a47822e1e97657a1c6cd07783816e3e955fa
Sanu Das <sanukr1202@gmail.com>
2023-05-05 15:49:55 +0530

    first commit

---
+++ src/content_option.js
@@ -0,0 +1,168 @@
+const logotext = "uWu";
+const meta = {
+    title: "Devyanshi",
+    description: "I an Devyanshi from LKO bls bla bla",
+};
+
+const introdata = {
+    title: "I am Devyanshi Singh",
+    animated: {
+        first: "I talk a lot. . .", 
+        second: "I have a boiprend. . .",
+        third: "I am annoying. . .",
+    },
+    description: "Aap yaha aye kislye?\n Let's sneak into my memories quickly. . .",
+    your_img_url: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234425/photo_2023-05-05_00-28-05_zh8vaz.jpg",
+};
+
+const dataabout = {
+    title: "About about my self",
+    aboutme: "Devyanshi is a talkative and fun-loving individual from Lucknow. She is currently pursuing MBBS in Delhi and has a passion for healthcare. Devyanshi enjoys exploring new places and trying out new restaurants, and has a great sense of humor.",
+};
+const worktimeline = [{
+        jobtitle: "Designer of week",
+        where: "YAdfi",
+        date: "2020",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "Jamalya",
+        date: "2019",
+    },
+    {
+        jobtitle: "Designer of week",
+        where: "ALquds",
+        date: "2019",
+    },
+];
+
+const skills = [{
+        name: "Anatomy",
+        value: 90,
+    },
+    {
+        name: "Physiology",
+        value: 85,
+    },
+    {
+        name: "Pharma",
+        value: 80,
+    },
+    {
+        name: "Bio-Chem",
+        value: 60,
+    },
+    {
+        name: "Genetics",
+        value: 85,
+    },
+];
+
+const services = [{
+        title: "UI & UX Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Mobile Apps",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+    {
+        title: "Wordpress Design",
+        description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed at nisl euismod urna bibendum sollicitudin.",
+    },
+];
+
+
+const dataportfolio = [{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683233915/photo_2023-05-05_00-28-00_qc7gky.jpg",
+    description: "Freshly rusted flowers",
+    link: "#",
+},
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234298/photo_2023-05-05_00-28-02_ku9mc8.jpg",
+    description: "Good/bad hair",
+    link: "#",
+},
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234334/photo_2023-05-05_00-28-03_e6gixy.jpg",
+    description: "Adrak <3",
+    link: "#",
+},
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234425/photo_2023-05-05_00-28-05_zh8vaz.jpg",
+    description: "After Hours",
+    link: "#",
+},
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234502/photo_2023-05-05_00-28-06_2_isagjm.jpg",
+    description: "Happy 20!",
+    link: "#",
+},
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234552/photo_2023-05-05_00-28-06_uqjpr8.jpg",
+    description: "Paneer!",
+    link: "#",
+},
+
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234610/photo_2023-05-05_00-28-07_oqorzs.jpg",
+    description: "Them",
+    link: "#",
+},
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234665/photo_2023-05-05_00-28-08_ac9qph.jpg",
+    description: "Us",
+    link: "#",
+},
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234711/photo_2023-05-05_00-28-09_u4y7te.jpg",
+    description: "White Hearts",
+    link: "#",
+},
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234755/photo_2023-05-05_00-28-11_vn8kvx.jpg",
+    description: "I Love 'em",
+    link: "#",
+},
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234814/photo_2023-05-05_00-44-31_csyasz.jpg",
+    description: "had to",
+    link: "#",
+},
+{
+    img: "https://res.cloudinary.com/dsy8swctr/image/upload/v1683234884/photo_2023-05-05_00-44-34_gndwjw.jpg",
+    description: "Star",
+    link: "#",
+},
+];
+
+
+const contactConfig = {
+    YOUR_EMAIL: "name@domain.com",
+    YOUR_FONE: "(555)123-4567",
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi vehicula eu nunc et sollicitudin. Cras pulvinar, nisi at imperdiet pharetra. ",
+    // creat an emailjs.com account 
+    // check out this tutorial https://www.emailjs.com/docs/examples/reactjs/
+    YOUR_SERVICE_ID: "service_id",
+    YOUR_TEMPLATE_ID: "template_id",
+    YOUR_USER_ID: "user_id",
+};
+
+const socialprofils = {
+    github: "https://github.com",
+    facebook: "https://facebook.com",
+    linkedin: "https://linkedin.com",
+    twitter: "https://twitter.com",
+};
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
